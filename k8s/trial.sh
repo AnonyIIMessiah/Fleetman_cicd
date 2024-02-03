741b3d4b1826eb03892f8302790064a744428548
AnonyIIMessiah <vaman1650.av@gmail.com>
2024-02-03 16:00:52 +0530

    check

--- k8s/trial.sh
+++ k8s/trial.sh
@@ -4,6 +4,7 @@ git clone https://github.com/AnonyIIMessiah/Fleetman_cicd.git
 cd Fleetman_cicd/k8s
 echo "Applying Changes in YAML file"
 kubectl apply -f . 
+kubectl set image deployment.apps/webapp webapp=anonymessiah/k8s-fleetmen-webapp-angular:latest
 cd ../..
 rm -rf Fleetman_cicd
 EOT
