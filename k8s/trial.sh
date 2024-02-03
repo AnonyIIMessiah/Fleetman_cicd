fe7c9df15d13944796a149cdea6b20ee4ae6460c
AnonyIIMessiah <vaman1650.av@gmail.com>
2024-02-03 11:38:40 +0530

    check

--- k8s/trial.sh
+++ k8s/trial.sh
@@ -2,7 +2,7 @@
 ssh -o "StrictHostKeyChecking no" -i "test.pem" ec2-user@18.213.1.85 "$( cat <<'EOT' 
 cd fleetman
 echo "Applying Changes in YAML file"
-'kubectl' apply -f . 
+kubectl apply -f . 
 whoami
 EOT
 )"
