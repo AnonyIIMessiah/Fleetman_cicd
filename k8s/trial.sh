34cdba983a92ad34de5b6a9d92f4d91d4072dd9a
AnonyIIMessiah <vaman1650.av@gmail.com>
2024-02-04 13:55:40 +0530

    check

--- k8s/trial.sh
+++ k8s/trial.sh
@@ -5,6 +5,10 @@ cd Fleetman_cicd/k8s
 echo "Applying Changes in YAML file"
 kubectl apply -f . 
 kubectl set image deployment.apps/webapp webapp=anonymessiah/k8s-fleetmen-webapp-angular:latest
+kubectl set image deployment.apps/position-tracker position-tracker=anonymessiah/k8s-fleetmen-position-simulator:latest
+kubectl set image deployment.apps/api-gateway api-gateway=anonymessiah/k8s-fleetmen-api-gateway:latest
+kubectl set image deployment.apps/position-simulator position-simulator=anonymessiah/k8s-fleetmen-position-simulator:latest
+
 cd ../..
 rm -rf Fleetman_cicd
 EOT
